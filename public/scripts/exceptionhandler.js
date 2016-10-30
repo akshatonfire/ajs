4d8ac3c1dbd75e89fe608aacc4c3aa193498f917
jprittie <eyewallop@gmail.com>
2016-10-29 20:29:50 -0400

    Update exceptionHandler

--- public/scripts/exceptionhandler.js
+++ public/scripts/exceptionhandler.js
@@ -5,8 +5,9 @@
   angular.module('exceptionOverwrite', []).
     factory('$exceptionHandler', ['$log', function($log) {
       return function myExceptionHandler(exception, cause) {
-        // exception.message;
-        $log.error(exception, cause);
+        exception.message;
+        alert(exception.message);
+        // $log.error(exception, cause);
       };
   }]);
 
