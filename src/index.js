29031eee0764e90b7c409ebc9dd75ef2d28333a3
jprittie <eyewallop@gmail.com>
2017-04-01 21:05:52 -0400

    Updates README formatting

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var port = process.env.PORT || 5000;
 
+
 app.use('/', express.static('public'));
 
 // vendor scripts
